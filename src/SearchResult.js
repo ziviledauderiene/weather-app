66888e062399fb89846a1d7f641d2b09fce3a126
ziviledauderiene <zivile.dauderiene@gmail.com>
2022-02-17 10:19:25 +0200

    currentVilnius, currentConditions improved

--- src/SearchResult.js
+++ src/SearchResult.js
@@ -12,23 +12,22 @@ const SearchResult = ({
   const getCurrentWeather = () => {
     (async () => {
       try {
-        const response = await fetch(urlCurrentWeather(searchResult.id), options);
-        var currentWeatherJSON = await response.json();
-        console.log(currentWeatherJSON);
-  
-        var currentConditions = [
-          { temp: currentWeatherJSON["current"]["temperature"] },
-          { feelsLikeTemp: currentWeatherJSON["current"]["feelsLikeTemp"] },
-          { symbol: currentWeatherJSON["current"]["symbol"] },
-          { symbolPhrase: currentWeatherJSON["current"]["symbolPhrase"] },
-          { wind: currentWeatherJSON["current"]["windSpeed"] },
-        ];
+        const response = await fetch(
+          urlCurrentWeather(searchResult.id),
+          options
+        );
+
+        const { current } = await response.json();
+        const currentConditions = {
+          temp: current.temperature,
+          feelsLikeTemp: current.feelsLikeTemp,
+          symbol: current.symbol,
+          symbolPhrase: current.symbolPhrase,
+          wind: current.windSpeed,
+          time: current.time,
+        };
         setCurrentConditions(currentConditions);
-        console.log(`Selected location: ${searchResult.name}`);
-        console.log(`Current weather conditions: ${currentConditions[0]["temp"]} °C, ${currentConditions[3]["symbolPhrase"]}`);
-        console.log(`${currentWeatherJSON["current"]["time"]}`);  
-      }
-      catch (error){
+      } catch (error) {
         console.log(error);
       }
     })();
@@ -38,32 +37,29 @@ const SearchResult = ({
     (async () => {
       try {
         const response = await fetch(urlForecast(searchResult.id), options);
-      var forecastJSON = await response.json();
+        var forecastJSON = await response.json();
 
-      var forecastData = [];
-      for (let a = 0; a < 7; a++) {
-        var day = [
-          { date: forecastJSON["forecast"][a]["date"] },
-          { maxTemp: forecastJSON["forecast"][a]["maxTemp"] },
-          { minTemp: forecastJSON["forecast"][a]["minTemp"] },
-          { symbol: forecastJSON["forecast"][a]["symbol"] },
-          { precipProb: forecastJSON["forecast"][a]["precipProb"] },
-        ];
-        forecastData.push(day);
-      }
-      setForecastData(forecastData);
-      setLoading(false);
-      document.body.style.backgroundImage =
-        "url('https://source.unsplash.com/1600x900/?" +
-        searchResult.name +
-        "')";
-      }
-      catch (error){
+        var forecastData = [];
+        for (let a = 0; a < 7; a++) {
+          var day = [
+            { date: forecastJSON["forecast"][a]["date"] },
+            { maxTemp: forecastJSON["forecast"][a]["maxTemp"] },
+            { minTemp: forecastJSON["forecast"][a]["minTemp"] },
+            { symbol: forecastJSON["forecast"][a]["symbol"] },
+            { precipProb: forecastJSON["forecast"][a]["precipProb"] },
+          ];
+          forecastData.push(day);
+        }
+        setForecastData(forecastData);
+        setLoading(false);
+        document.body.style.backgroundImage =
+          "url('https://source.unsplash.com/1600x900/?" +
+          searchResult.name +
+          "')";
+      } catch (error) {
         console.log(error);
       }
-       
     })();
-
   };
 
   return (
@@ -81,7 +77,6 @@ const SearchResult = ({
       {searchResult.name}, {searchResult.adminArea}, {searchResult.country}
     </p>
   );
-
 };
 
 export default SearchResult;
